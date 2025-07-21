af689a0f3eac0dc18104663d787f373aaf60d92d
Levan Didebashvili <levdidebashvili@gmail.com>
2025-07-21 20:04:20 +0400

    bugfix/
    bugfix/ shrinking when <393px

--- feature/landing/components/composities/Footer.tsx
+++ feature/landing/components/composities/Footer.tsx
@@ -3,8 +3,8 @@ import Image from "next/image"
 
 const Footer = () => {
   return (
-    <footer className="mx-auto flex w-full flex-row items-center justify-between border-t border-gray-300 px-4 py-6 lg:h-[240px] lg:max-w-[1440px] lg:px-10">
-      <div className="flex w-1/3 justify-start lg:ml-[32px]">
+    <footer className="mx-auto flex w-full flex-row items-center justify-between border-t border-[#A8A8A8] px-[32px] py-[40px] lg:h-[240px] lg:max-w-[1440px] lg:px-10 lg:py-6">
+      <div className="flex min-w-[90px] justify-start lg:ml-[32px]">
         <div className="block lg:hidden">
           <Image
             alt="Logo"
@@ -62,7 +62,7 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="flex w-2/3 flex-col gap-2 lg:hidden">
+      <div className="flex w-full min-w-[320px] flex-col gap-2 px-2 lg:hidden">
         <div className="flex items-center justify-between gap-2">
           <span className="text-[12px] text-[#575757]">
             10 Dodo Abashidze St T'bilisi
@@ -79,7 +79,9 @@ const Footer = () => {
         </div>
 
         <div className="flex items-center justify-between gap-2">
-          <span className="text-[12px] text-[#575757]">+995 567 567 567</span>
+          <span className="text-[12px] whitespace-nowrap text-[#575757]">
+            +995 567 567 567
+          </span>
           <div className="flex items-center gap-[4px] text-[10px] text-[#575757]">
             <Image
               alt="Instagram"
--- feature/landing/components/composities/Landing.tsx
+++ feature/landing/components/composities/Landing.tsx
@@ -5,8 +5,8 @@ import Footer from "./Footer"
 const Landing = () => {
   return (
     <div>
-      <Header />
-      {/* <Footer /> */}
+      {/* <Header /> */}
+      <Footer />
     </div>
   )
 }
