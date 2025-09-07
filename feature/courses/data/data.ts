3a80021b87b085a6ac998d1445531f5a038aab8c
MtchedlishviliLevani <levan.mtchedlishvili.5@iliauni.edu.ge>
2025-09-07 10:43:40 +0400

    chore/add course assets and data

---
+++ feature/courses/data/data.ts
@@ -0,0 +1,42 @@
+import course1 from "../../courses/assets/course1.png";
+import course2 from "../../courses/assets/course2.png";
+import course3 from "../../courses/assets/course3.png";
+import course4 from "../../courses/assets/course4.png";
+
+export const courseCardsContent = [{
+    image: course1,
+    title: "Individual Consultation",
+    text:"Receive personalized feedback and expert guidance to refine your creative process and unlock your artistic potential",
+    id:"1"
+}, {
+    image: course2,
+    title: "Group Course (Art as Practice)",
+    text:"Reconnect with your natural creativity through an immersive drawing course that encourages exploration and self-expression",
+    id:"2"
+}, {
+    image: course3,
+    title:"Children’s Course",
+    text:"A hands-on creative program designed to introduce young artists to visual composition, artistic research, and personal project development",
+    id:"3"
+}, {
+    image: course4,
+    title: "Corporate Offerings",
+    text:"Strengthen team collaboration and innovation with a unique creative workshop that fosters artistic freedom and shared experience",
+    id:"4"
+}]
+
+
+export const consultationPoints = [
+  {
+    title: "Customized guidance",
+    description: "to support your artistic growth",
+  },
+  {
+    title: "Flexible formats",
+    description: "one-time or long-term collaboration",
+  },
+  {
+    title: "Actionable insights",
+    description: "to elevate your work",
+  },
+];
