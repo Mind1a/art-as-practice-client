56e13c6db20de37f20e23a93d11b67e44f5f7049
AchiGel <ar.gelashvili@gmail.com>
2025-09-12 12:49:12 +0400

    hotfix/add hover

--- feature/shop/components/ItemPurchase.tsx
+++ feature/shop/components/ItemPurchase.tsx
@@ -8,7 +8,7 @@ const ItemPurchase = () => {
         <Image width={24} height={24} src={copyImage} alt="copy button" />
         <span className="font-semibold text-[#F2430D] text-xl">ID879</span>
       </div>
-      <button className="bg-[#F2430D] px-4 py-3 rounded-lg font-semibold text-[15px] text-white cursor-pointer">
+      <button className="bg-[#F2430D] hover:bg-[#B74C2C] px-4 py-3 rounded-lg font-semibold text-[15px] text-white transition-all cursor-pointer">
         Buy It Now
       </button>
     </div>
