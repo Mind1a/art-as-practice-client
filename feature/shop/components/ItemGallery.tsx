8f976c936fa6100c3a46c0ef6f7c44d55b870fd1
AchiGel <ar.gelashvili@gmail.com>
2025-09-12 12:43:31 +0400

    fix/ItemGallery - add images change feature

--- feature/shop/components/ItemGallery.tsx
+++ feature/shop/components/ItemGallery.tsx
@@ -1,12 +1,16 @@
+"use client";
+
 import Image from "next/image";
 
 import image1 from "@/feature/shop/assets/gallery/image-1.png";
 import image2 from "@/feature/shop/assets/gallery/image-2.png";
 import image3 from "@/feature/shop/assets/gallery/image-3.png";
 import image4 from "@/feature/shop/assets/gallery/image-4.png";
+import { useState } from "react";
 
 const ItemGallery = () => {
   const images = [image1, image2, image3, image4];
+  const [mainImage, setMainImage] = useState(images[0]);
 
   return (
     <div className="flex md:flex-row flex-col lg:items-center gap-4 md:gap-6 mx-auto lg:mb-8 w-full max-w-[720px] lg:max-w-full md:max-h-[670px] lg:max-h-full">
@@ -14,7 +18,7 @@ const ItemGallery = () => {
       <div className="flex-1 px-7 md:px-0 w-full lg:max-w-[680px] md:max-h-full">
         <Image
           className="w-full h-full object-cover"
-          src={image1}
+          src={mainImage}
           alt="Main image"
           width={800}
           height={500}
@@ -32,6 +36,7 @@ const ItemGallery = () => {
             alt={`Gallery image ${i + 1}`}
             width={250}
             height={200}
+            onClick={() => setMainImage(img)}
           />
         ))}
       </div>
