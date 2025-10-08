55722bdcc6d7b10b512030d9bb194ec81956fef7
Mindia Arabuli <mindia.arabuli@iliauni.edu.ge>
2025-10-08 16:55:10 +0400

    Refactor for locale-based routing and cleanup
    Moved course, shop, and FAQ pages under a locale-based directory to support internationalization. Removed old non-locale pages and updated imports for consistency. Also enabled the Header component on the landing page and made minor code style improvements.

--- api/artworksApi.ts
+++ api/artworksApi.ts
@@ -1,23 +1,23 @@
-import { baseUrl } from "@/feature/shop/components/composites/Shop";
-import { ArtworkTypes } from "@/feature/shop/lib/shopTypes";
-import { ParamValue } from "next/dist/server/request/params";
+import { baseUrl } from "feature/shop/components/composites/Shop"
+import { ArtworkTypes } from "feature/shop/lib/shopTypes"
+import { ParamValue } from "next/dist/server/request/params"
 
 export const getAllArtworks = async ({
   page,
   limit,
 }: {
-  page: number;
-  limit: number;
+  page: number
+  limit: number
 }): Promise<ArtworkTypes[]> => {
-  const res = await fetch(`${baseUrl}/Artworks/?page=${page}&limit=${limit}`);
-  if (!res.ok) throw new Error("Failed to fetch artworks");
-  return res.json();
-};
+  const res = await fetch(`${baseUrl}/Artworks/?page=${page}&limit=${limit}`)
+  if (!res.ok) throw new Error("Failed to fetch artworks")
+  return res.json()
+}
 
 export const getSingleArtwork = async (
   id: ParamValue
 ): Promise<ArtworkTypes> => {
-  const res = await fetch(`${baseUrl}/Artworks/${id}`);
-  if (!res.ok) throw new Error("Failed to fetch artwork");
-  return res.json();
-};
+  const res = await fetch(`${baseUrl}/Artworks/${id}`)
+  if (!res.ok) throw new Error("Failed to fetch artwork")
+  return res.json()
+}
--- feature/landing/components/composities/Landing.tsx
+++ feature/landing/components/composities/Landing.tsx
@@ -5,7 +5,7 @@ import Footer from "./Footer"
 const Landing = () => {
   return (
     <div>
-      {/* <Header /> */}
+      <Header />
       <Footer />
     </div>
   )
