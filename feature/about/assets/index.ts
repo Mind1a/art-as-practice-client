1f522c605ab1c9a3ce3464c5948cf38ba75c0760
Davit Kvinikadze <160760373+DataKvinikadze@users.noreply.github.com>
2026-01-25 15:58:10 +0400

    feature/Add SVG assets and update exports in assets index
    Added four new SVG files (Vector-1, Vector-left, vector-2, vector-right) to the assets directory. Updated index.ts to import and export these SVGs, and included them in the Images.svgs array for easier access throughout the feature.

--- feature/about/assets/index.ts
+++ feature/about/assets/index.ts
@@ -4,11 +4,26 @@ import slide1 from "./slider/slide-1.png"
 import slide2 from "./slider/slide-2.png"
 import slide3 from "./slider/slide-3.png"
 
+import vector1 from "./svgs/Vector-1.svg"
+import vector2 from "./svgs/Vector-2.svg"
+import vectorLeft from "./svgs/Vector-Left.svg"
+import vectorRight from "./svgs/Vector-Right.svg"
+
 export const Images = {
   mentors: {
     main: mentorMain,
   },
   slider: [slide1, slide2, slide3],
+  svgs: [vector1, vector2, vectorLeft, vectorRight],
 }
 
-export { mentorMain, slide1, slide2, slide3 }
+export {
+  mentorMain,
+  slide1,
+  slide2,
+  slide3,
+  vector1,
+  vector2,
+  vectorLeft,
+  vectorRight,
+}
